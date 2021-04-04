4069069d99fef85e8c1cb3cf868e402fbb1b6ffa
KtishnaChaitanyaSuroj <78598527+KtishnaChaitanyaSuroj@users.noreply.github.com>
2021-04-04 17:33:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+var helicopterIMG, helicopterSprite, packageSprite, box1, box2, box3, Box1, packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+     Box1 = createSprite(width/2-140,height-90,20,100);
+     Box1.shapeColor = "red";
+
+	 Box2 = createSprite(width/2-40,height-55,200,30);
+     Box2.shapeColor = "red";
+
+	 Box3 = createSprite(width/2+60,height-90,20,100);
+	 Box3.shapeColor = "red";
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+    box1 = Bodies.rectangle(width/2-140, height-90, 20,100,{isStatic:true});
+	World.add(world, box1);
+
+	box2 = Bodies.rectangle(width/2,height-55,200,30,{isStatic:true});
+	World.add(world, box2);
+
+	box3 = Bodies.rectangle(width/2+100,height-90,20,100,{isStatic:true});
+    World.add(world, box3);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  drawSprites();
+ 
+  
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody, false);
+  }
+}
+
+
+
